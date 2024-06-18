565ae4a73ec9a6241fac7f9101d126e7850c1072
Thuan Doan <doantrananhthuan@gmail.com>
2024-06-18 20:52:35 +0700

    commit 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-// function load(selector, path) {
-//   const cached = localStorage.getItem(path);
-//   if (cached) {
-//     document.querySelector(selector).innerHTML = cached;
-//   }
-
-//   fetch(path)
-//     .then((res) => res.text())
-//     .then((html) => {
-//       if (html !== cached) {
-//         document.querySelector(selector).innerHTML = html;
-//         localStorage.setItem(path, html);
-//       }
-//     });
-// }
-
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
@@ -130,13 +114,19 @@ function handleActiveMenu() {
       if (!items.length) return;
 
       removeActive(menu);
-      items[0].classList.add(activeClass);
+      if (window.innerWidth > 991) items[0].classList.add(activeClass);
 
       Array.from(items).forEach((item) => {
         item.onmouseenter = () => {
           if (window.innerWidth <= 991) return;
           removeActive(menu);
           item.classList.add(activeClass);
+        };
+        item.onclick = () => {
+          if (window.innerWidth > 991) return;
+          removeActive(menu);
+          item.classList.add(activeClass);
+          item.scrollIntoView();
         };
       });
     });
@@ -156,24 +146,36 @@ function handleActiveMenu() {
  * <button class="js-toggle" toggle-target="#box">Click</button>
  * <div id="box">Content show/hide</div>
  */
-window.addEventListener("template-loaded", initJsToggle);
+window.addEventListener('template-loaded', initJsToggle);
 
 function initJsToggle() {
-    $$(".js-toggle").forEach((button) => {
-        const target = button.getAttribute("toggle-target");
-        if (!target) {
-            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
-        }
-        button.onclick = () => {
-            if (!$(target)) {
-                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
-            }
-            const isHidden = $(target).classList.contains("hide");
+  $$('.js-toggle').forEach((button) => {
+    const target = button.getAttribute('toggle-target');
+    if (!target) {
+      document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+    }
+    button.onclick = () => {
+      if (!$(target)) {
+        return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+      }
+      const isHidden = $(target).classList.contains('hide');
 
-            requestAnimationFrame(() => {
-                $(target).classList.toggle("hide", !isHidden);
-                $(target).classList.toggle("show", isHidden);
-            });
-        };
-    });
+      requestAnimationFrame(() => {
+        $(target).classList.toggle('hide', !isHidden);
+        $(target).classList.toggle('show', isHidden);
+      });
+    };
+  });
 }
+
+window.addEventListener('template-loaded', () => {
+  const links = $$('.js-dropdown-list > li > a');
+
+  links.forEach((link) => {
+    link.onclick = () => {
+      if (window.innerWidth > 991) return;
+      const item = link.closest('li');
+      item.classList.toggle('navbar__item--active');
+    };
+  });
+});
